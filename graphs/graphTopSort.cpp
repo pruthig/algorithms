4164b0f4c6e9c6b15c3b27fcedf6f15779d58c36
Gaurav Pruthi <gaurav.pruthi88@gmail.com>
2015-11-06 19:48:54 +0530

    Topological Sort

---
+++ graphs/graphTopSort.cpp
@@ -0,0 +1,105 @@
+#include<vector>
+#include<cstdlib>
+#include<stack>
+#include<queue>
+#include<iostream>
+
+using std::vector;
+using std::cin;
+using std::endl;
+using std::cout;
+using std::stack;
+using std::queue;
+
+class Graph {
+
+    //A vector of vector...graph[i] denotes the vector of edges that are connected to graph[i]
+    vector< vector<int> > graphVector;
+    stack<int> stck, reverse;
+    bool *marked;
+    int vertexCount;
+public :
+    void addVertex(int v); 
+    void init(int v); 
+    void dfsWithStack(int v); 
+    void topSort();
+    void addEdge(int v, int w);
+};
+
+void Graph::init(int a){
+    vertexCount = a;
+    marked = new bool[a];
+    for(int i = 0 ; i < a; ++i)
+        marked[i] = false;
+    addVertex(a);
+}
+
+
+void Graph::addEdge(int v, int w) {
+  graphVector[v].push_back(w);
+}
+
+void Graph::addVertex(int v) {
+    for(int i = 0; i < v; ++i) {
+        vector<int> a;
+        a.clear();
+    	graphVector.push_back(a);
+    }
+}
+
+
+void Graph::dfsWithStack(int vertex) {
+    
+    marked[vertex] = true;
+    vector<int> vect = graphVector[vertex];
+    for(int w : vect) {
+        
+        if(!marked[w]) {
+            dfsWithStack(w);
+        }
+
+    }
+
+    reverse.push(vertex);
+
+}
+
+void Graph::topSort() {
+    for(int  i = 0; i < vertexCount; ++i) {
+        if(!marked[i]) {
+            dfsWithStack(i);
+        }
+    }
+    
+    while(!reverse.empty()){
+        cout<<", "<<reverse.top()<<", ";
+        reverse.pop();
+    }
+}
+
+
+//To cover DFS we need to have double entries for edges: x,y and y,x
+int main(){
+	Graph gObject;
+    gObject.init(7);
+
+	gObject.addEdge(0, 1);
+	gObject.addEdge(0, 2);
+	gObject.addEdge(0, 5);
+
+	gObject.addEdge(1, 4);
+
+	gObject.addEdge(3, 2);
+	gObject.addEdge(3, 5);
+	gObject.addEdge(3, 6);
+	gObject.addEdge(3, 4);
+
+	gObject.addEdge(5, 2);
+
+	gObject.addEdge(6, 0);
+	gObject.addEdge(6, 4);
+
+    gObject.topSort();
+    cout<<endl;
+	return 0;
+}
