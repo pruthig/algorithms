2a63be983393e94fc663ea60c6217112879c06e6
Gaurav Pruthi <gaurav.pruthi@gemalto.com>
2018-01-25 16:41:34 +0530

    temp files

--- coursera/Python/Week4/ProblemSet4.py
+++ coursera/Python/Week4/ProblemSet4.py
@@ -22,7 +22,9 @@ firstline = ["Happy", "families", "are", "all", "alike;", "every", \
 #%%
 def problem4_1(wordlist):
     """ Takes a word list prints it, sorts it, and prints the sorted list """
-    pass # replace this pass (a do-nothing) statement with your code
+    print(wordlist)
+    wordlist.sort(key=str.lower)
+    print(wordlist)
     
 #%%
 """
@@ -40,6 +42,8 @@ problem4_2(numList)
 """
 #%%
 import random
+import statistics
+
 numList = []
 random.seed(150)
 for i in range(0,25):
@@ -47,7 +51,8 @@ for i in range(0,25):
 #%%   
 def problem4_2(ran_list):
     """ Compute the mean and standard deviation of a list of floats """
-    pass # replace this pass (a do-nothing) statement with your code
+    print(statistics.mean(ran_list))
+    print(statistics.stdev(ran_list))
 #%%
 """
 Problem 4_3:
@@ -66,7 +71,7 @@ toothbrush               $  2.60
 def problem4_3(product, cost):
     """ Prints the product name in a space of 25 characters, left-justified
         and the price in a space of 6 characters, right-justified"""
-    pass # replace this pass (a do-nothing) statement with your code
+    print("{0:<25}$ {1:>6}".format(product, cost))
 
 
 #%%    
@@ -118,3 +123,143 @@ Choice: s
   3  Jerry Seinfeld          (212) 842-2527
 
 """
+
+import os
+import csv
+
+
+phones = []
+name_pos = 0
+phone_pos = 1
+phone_header = [ 'Name', 'Phone Number']
+
+def proper_menu_choice(which):
+    if not which.isdigit():
+        print ("'" + which + "' needs to be the number of a phone!")
+        return False
+    which = int(which)
+    if which < 1 or which > len(phones):
+        print ("'" + str(which) + "' needs to be the number of a phone!")
+        return False
+    return True
+    
+def delete_phone(which):
+    if not proper_menu_choice(which):
+        return
+    which = int(which)
+
+    del phones[which-1]
+    print( "Deleted phone #", which)
+
+def edit_phone(which):
+    if not proper_menu_choice(which):
+        return
+    which = int(which)
+        
+    phone = phones[which-1]
+    print("Enter the data for a new phone. Press <enter> to leave unchanged.")
+    
+    print(phone[name_pos])
+    newname = input("Enter phone name to change or press return: ")
+    if newname == "":
+        newname = phone[name_pos]
+        
+    print(phone[phone_pos])    
+    newphone_num = input("Enter new phone number to change or press return: ")
+    if newphone_num == "":
+        newphone_num = phone[phone_pos]
+            
+    phone = [newname, newphone_num]
+    phones[which-1] = phone
+
+  
+def save_phone_list():
+
+    f = open("myphones.csv", 'w', newline='')
+    for item in phones:
+        csv.writer(f).writerow(item)
+    f.close()
+  
+def load_phone_list():
+    if os.access("myphones.csv",os.F_OK):
+        f = open("myphones.csv")
+        for row in csv.reader(f):
+            phones.append(row)
+        f.close()
+
+     
+def reorder_phones():
+    phones.sort(key=lambda phone: phone[0].lower())
+
+    
+    
+def show_phones():
+    show_phone(phone_header, "")
+    index = 1
+    for phone in phones:
+        show_phone(phone, index)
+        index = index + 1
+    print()
+
+def show_phone(phone, index):
+    outputstr = "{0:>3}  {1:<20}  {2:>16}"
+    print(outputstr.format(index, phone[name_pos], phone[phone_pos]))
+
+def create_phone():
+    print("Enter the data for a new phone:")
+    newname = input("Enter name: ")
+    newphone_num = input("Enter phone number: ")
+    phone = [newname,newphone_num]
+    phones.append(phone)
+    
+def menu_choice():
+    """ Find out what the user wants to do next. """
+    print("Choose one of the following options?")
+    print("   s) Show")
+    print("   n) New")
+    print("   r) Reorder")
+    print("   d) Delete")
+    print("   e) Edit")
+    print("   q) Quit")
+    choice = input("Choice: ")    
+    if choice.lower() in ['n','d', 'r', 's','e', 'q']:
+        return choice.lower()
+    else:
+        print(choice +"?")
+        print("Invalid option")
+        return None
+
+
+def main_loop():
+    
+    load_phone_list()
+    
+    while True:
+        choice = menu_choice()
+        if choice == None:
+            continue
+        if choice == 'q':
+            print( "Exiting...")
+            break     # jump out of while loop
+        elif choice == 'n':
+            create_phone()
+        elif choice == 'r':
+            reorder_phones()
+        elif choice == 'd':
+            which = input("Which item do you want to delete? ")
+            print("which is ", which)
+            delete_phone(which)
+        elif choice == 's':
+            show_phones()
+        elif choice == 's':
+            reorder_phones()
+        elif choice == 'e':
+            which = input("Which item do you want to edit? ")
+            print("which is ", which)
+            edit_phone(which)
+        else:
+            print("Invalid choice.")
+            
+    save_phone_list()
+    
+
