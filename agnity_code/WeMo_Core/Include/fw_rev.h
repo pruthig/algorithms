78de5984d524bee713e3f800fa4f329589682c80
Gaurav Pruthi <gaurav.pruthi88@gmail.com>
2019-09-12 14:10:37 +0530

    Deleted WeMo code

--- agnity_code/WeMo_Core/Include/fw_rev.h
+++
@@ -1,7 +0,0 @@
-#ifndef __FW__REV__H__
-#define __FW__REV__H__
-#define BUILD_TIME "Friday 12 September 2014 11:42:27  IST"
-#define FW_REV "WeMo_WW_2.00.6851.DVT"
-#define FW_REV1 "2.00.6851"
-#endif
-
--- agnity_code/WeMo_Core/target/gemtek_api_LED/gemtek_api_LED.h
+++
@@ -1,8 +0,0 @@
-//#include "ixp4xx_gpio.h"
-//#include "gemtek_function.h"
-
-#define Gemtek_Success 0
-#define Gemtek_Error -1
-
-int SetActivityLED(int state);
-
