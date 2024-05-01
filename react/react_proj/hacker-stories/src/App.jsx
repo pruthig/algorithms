5f6cf2ee5d4187035b48b9dabd06185b3845ca49
Gaurav Pruthi <gaurav.pruthi88@gmail.com>
2024-05-01 14:57:22 +0530

    Added UI for Springboot

---
+++ react/react_proj/hacker-stories/src/App.jsx
@@ -0,0 +1,159 @@
+import * as React from 'react';
+
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const useStorageState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
+const getAsyncStories = () =>
+  new Promise((resolve) =>
+    setTimeout(
+      () => resolve({ data: { stories: initialStories } }),
+      2000
+)
+);
+
+const App = () => {
+
+  const [searchTerm, setSearchTerm] = useStorageState(
+    'search',
+    'React'
+  );
+
+  const [stories, setStories] = React.useState(initialStories);
+  const [isLoading, setIsLoading] = React.useState(true)
+  const [isError, setIsError] = React.useState(false);
+  React.useEffect(()=> {
+    setIsLoading(true);
+    getAsyncStories().then((result) => {
+    setStories(result.data.stories);
+    setIsLoading(false);
+  })
+  .catch(() => setIsError(true));
+}, []);
+
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+
+    setStories(newStories);
+  };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  if (isLoading) {
+    return <p>Loading ...</p>;
+  }
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        isFocused
+        onInputChange={handleSearch}
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
+
+      <hr />
+
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+    </div>
+  );
+};
+
+const InputWithLabel = ({
+  id,
+  value,
+  type = 'text',
+  onInputChange,
+  isFocused,
+  children,
+}) => {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
+
+  const List = ({ list, onRemoveItem })  => (
+
+    <ul>
+      {
+      list.map((item) => (
+        <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} /> )
+        )
+      }
+  </ul> 
+  );
+  
+
+  const Item = ({ item, onRemoveItem }) => (
+    <li>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+      <span>
+        <button type="button" onClick={() => onRemoveItem(item)}>
+          Dismiss
+        </button>
+      </span>
+    </li>
+  );
+
+export default App;
